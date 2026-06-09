afc1fc351aa367de6740ef903b7d33c5461b5bde
Sivadarani <sivadaranishivani382006@gmail.com>
2026-06-09 11:39:07 +0530

    Added linked list practice programs

---
+++ Main1.java
@@ -0,0 +1,136 @@
+class Node {
+    int data;
+    Node prev, next;
+
+    Node(int data) {
+        this.data = data;
+        prev = null;
+        next = null;
+    }
+}
+
+public class Main1 {
+
+    static Node head = null;
+
+    // Insert at Beginning
+    static void insertBeginning(int data) {
+        Node newNode = new Node(data);
+
+        if (head != null) {
+            head.prev = newNode;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insert at End
+    static void insertEnd(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    // Delete First Node
+    static void deleteBeginning() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        head = head.next;
+
+        if (head != null) {
+            head.prev = null;
+        }
+    }
+
+    // Delete Last Node
+    static void deleteEnd() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.prev.next = null;
+    }
+
+    // Display Forward
+    static void displayForward() {
+        Node temp = head;
+
+        System.out.print("Forward: ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    // Display Backward
+    static void displayBackward() {
+        if (head == null) {
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        System.out.print("Backward: ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        insertEnd(10);
+        insertEnd(20);
+        insertEnd(30);
+
+        displayForward();
+        displayBackward();
+
+        insertBeginning(5);
+
+        displayForward();
+
+        deleteBeginning();
+
+        displayForward();
+
+        deleteEnd();
+
+        displayForward();
+        displayBackward();
+    }
+}
