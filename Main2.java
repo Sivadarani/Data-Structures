24f0da72f234fa7d8a0936b257900fd7d565b025
Sivadarani <sivadaranishivani382006@gmail.com>
2026-06-10 10:08:32 +0530

    Added linked list practice programs

---
+++ Main2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main2{
+    public static void main(String[] args){
+        Stack<Integer> s=new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        System.out.println("Stack : "+s);
+        System.out.println("Stack top element: "+s.peek());
+        System.out.println("Stack  : "+s);
+        System.out.println("Stack after popped : "+s.pop());
+        System.out.println("Stack : "+s);
+        System.out.println("Stack : "+s.size());
+        System.out.println("Stack is empty : "+s.empty());
+
+
+    }
+}
